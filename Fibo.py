2e882eb212100449dc34ecf0546bf8bf2487b204
maheravi <56476428+maheravi@users.noreply.github.com>
2021-04-06 20:29:32 +0430

    Add files via upload

---
+++ Fibo.py
@@ -0,0 +1,21 @@
+def seqfibo(n):
+    if n <= 1:
+        return n
+    else:
+        return seqfibo(n - 1) + seqfibo(n - 2)
+
+
+fibo = list()
+
+
+def show(terms):
+    print("Fibonacci sequence:")
+    for i in range(terms):
+        a = seqfibo(i)
+        fibo.append(a)
+        Fibo = tuple(fibo)
+    print(Fibo)
+
+
+terms = int(input("Please enter number of sequence= "))
+show(terms)
